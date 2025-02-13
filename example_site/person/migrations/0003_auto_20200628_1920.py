c3a7e0bfa8e5d2da1fa7c9b29a5d4d6091fce3d8
Ryan Malloy <ryan@supported.systems>
2025-02-13 10:32:16 -0700

    add optional timezone lookup support and US Census GeoCoder Support (w/cached lookups)

--- example_site/person/migrations/0003_auto_20200628_1920.py
+++ example_site/person/migrations/0003_auto_20200628_1920.py
@@ -9,7 +9,6 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ("address", "0002_auto_20160213_1726"),
-        ("person", "0002_auto_20200628_1720"),
     ]
 
     operations = [
