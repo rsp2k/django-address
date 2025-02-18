19b2bfd392b67688108b5a6210aa26a29350b0f4
Ryan Malloy <ryan@supported.systems>
2025-02-17 23:46:17 -0700

    add optional timezone lookup support and US Census GeoCoder Support (w/cached lookups)

--- address/migrations/0004_alter_person_id.py
+++ address/migrations/0004_alter_person_id.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("address", "0003_auto_20200628_1920"),
+        ("address", "0003_auto_20200830_1851"),
     ]
 
     operations = [
